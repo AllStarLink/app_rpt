49cb56701be87f218131bc2257e23d4421b7592c
InterLinked1 <24227567+InterLinked1@users.noreply.github.com>
2022-08-08 20:20:56 -0400

    install: add shell script to apply patches and install
    Add script to automatically apply repo differentials to current Asterisk install,
    install any needed pre-reqs, and recompile and install. This will make it easy
    for people to install all this since you can just run this script after running
    PhreakScript to install base Asterisk and DAHDI.

---
+++ rpt_install.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+cd /usr/src
+
+# Download app_rpt repo if not present already
+if [ ! -d app_rpt ]; then
+	git clone https://github.com/InterLinked1/app_rpt.git
+else
+	# else, update it
+	cd app_rpt
+	git checkout master
+	git pull
+	cd ..
+fi
+
+# cd into Asterisk source directory
+cd $( ls -d -v */ | grep "^asterisk" | tail -1 )
+
+apt-get install -y libusb-dev # chan_simpleusb requires libusb-dev on Debian
+modprobe snd-pcm-oss # /dev/dsp1 needs to exist for chan_simpleusb and chan_usbradio to work
+
+wget "https://code.phreaknet.org/asterisk/rpt.diff" -O /tmp/rpt.diff
+git apply /tmp/rpt.diff
+
+echoerr() {
+	printf "\e[31;1m%s\e[0m\n" "$*" >&2;
+}
+
+rpt_add() {
+	if [ ! -f ../app_rpt/$1 ]; then
+		echoerr "WARNING: File $1 does not exist"
+	fi
+	cp ../app_rpt/$1 $1
+}
+
+if [ ! -d apps/app_rpt ]; then
+	mkdir apps/app_rpt
+fi
+if [ ! -d channels/xpmr ]; then
+	mkdir channels/xpmr
+fi
+
+rpt_add "apps/app_rpt.c"
+rpt_add "apps/app_gps.c"
+rpt_add "apps/app_rpt/app_rpt.h"
+rpt_add "apps/app_rpt/mdc_encode.c"
+rpt_add "apps/app_rpt/mdc_encode.h"
+rpt_add "apps/app_rpt/mdc_decode.c"
+rpt_add "apps/app_rpt/mdc_decode.h"
+rpt_add "apps/app_rpt/pocsag.c"
+rpt_add "apps/app_rpt/pocsag.h"
+rpt_add "apps/app_rpt/rpt_cli.h"
+rpt_add "apps/app_rpt/rpt_daq.c"
+rpt_add "apps/app_rpt/rpt_daq.h"
+rpt_add "apps/app_rpt/rpt_lock.c"
+rpt_add "apps/app_rpt/rpt_lock.h"
+rpt_add "apps/app_rpt/rpt_utils.c"
+rpt_add "apps/app_rpt/rpt_utils.h"
+rpt_add "channels/chan_simpleusb.c"
+rpt_add "channels/chan_voter.c"
+rpt_add "channels/xpmr/sinetabx.h"
+rpt_add "channels/xpmr/xpmr.c"
+rpt_add "channels/xpmr/xpmr.h"
+rpt_add "channels/xpmr/xpmr_coef.h"
+
+make apps
+make channels
+make install
