5f8402d4fa225ddc4eca05abf260594147a1456c
Danny Lloyd <kb4mdd@arrl.net>
2023-04-12 14:02:29 -0500

    chan_echolink: Do not double free frame used in ast_dsp_process (#148)
    chan_echolink is freeing a frame that was already freed by ast_dsp_process. This fixes #147.

--- channels/chan_echolink.c
+++ channels/chan_echolink.c
@@ -1989,7 +1989,6 @@ static int el_xwrite(struct ast_channel *ast, struct ast_frame *frame)
 			if (p->dsp && (!instp->useless_flag_1)) {
 				f2 = ast_translate(p->xpath, &fr, 0);
 				f1 = ast_dsp_process(NULL, p->dsp, f2);
-				ast_frfree(f2);
 				if ((f1->frametype == AST_FRAME_DTMF_END) || (f1->frametype == AST_FRAME_DTMF_BEGIN))
 				{
 					if ((f1->subclass.integer != 'm') && (f1->subclass.integer != 'u')) {
