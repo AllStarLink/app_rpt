1108fd3f992d560e951b65deb8a1befc9705fd42
InterLinked1 <24227567+InterLinked1@users.noreply.github.com>
2024-04-29 10:03:56 -0400

    chan_beagle: Initial port to compile on current Asterisk.
    There are still a few unresolved references to SND_PCCV
    stuff on my machine, but otherwise, this updates the
    code for current Asterisk APIs. The sample config
    has been moved out of the module itself into its own
    sample file.
    
    Resolves: #280
    
    Rebase original chan_beagle in preparation for building and archival.

--- rpt_install.sh
+++ rpt_install.sh
@@ -63,6 +63,7 @@ fi
 # Remove anything that may have been there before (mainly relevant for testing older HEADs)
 rm -f apps/app_rpt/*
 
+<<<<<<< HEAD
 # Copy in the radio modules and files
 cp ../$MYDIR/apps/*.c apps
 cp ../$MYDIR/apps/app_rpt/* apps/app_rpt
@@ -72,6 +73,78 @@ cp ../$MYDIR/configs/samples/*.conf.sample configs/samples
 cp ../$MYDIR/include/asterisk/*.h include/asterisk
 cp ../$MYDIR/res/* res
 cp ../$MYDIR/utils/*.c utils
+=======
+rpt_add "apps/app_rpt.c"
+rpt_add "apps/app_gps.c"
+rpt_add "apps/app_rpt/app_rpt.h"
+rpt_add "apps/app_rpt/mdc_encode.c"
+rpt_add "apps/app_rpt/mdc_encode.h"
+rpt_add "apps/app_rpt/mdc_decode.c"
+rpt_add "apps/app_rpt/mdc_decode.h"
+rpt_add "apps/app_rpt/rpt_mdc1200.c"
+rpt_add "apps/app_rpt/rpt_mdc1200.h"
+rpt_add "apps/app_rpt/pocsag.c"
+rpt_add "apps/app_rpt/pocsag.h"
+rpt_add "apps/app_rpt/rpt_cli.c"
+rpt_add "apps/app_rpt/rpt_cli.h"
+rpt_add "apps/app_rpt/rpt_daq.c"
+rpt_add "apps/app_rpt/rpt_daq.h"
+rpt_add "apps/app_rpt/rpt_lock.c"
+rpt_add "apps/app_rpt/rpt_lock.h"
+rpt_add "apps/app_rpt/rpt_utils.c"
+rpt_add "apps/app_rpt/rpt_utils.h"
+rpt_add "apps/app_rpt/rpt_call.c"
+rpt_add "apps/app_rpt/rpt_call.h"
+rpt_add "apps/app_rpt/rpt_serial.c"
+rpt_add "apps/app_rpt/rpt_serial.h"
+rpt_add "apps/app_rpt/rpt_xcat.c"
+rpt_add "apps/app_rpt/rpt_xcat.h"
+rpt_add "apps/app_rpt/rpt_capabilities.c"
+rpt_add "apps/app_rpt/rpt_capabilities.h"
+rpt_add "apps/app_rpt/rpt_vox.c"
+rpt_add "apps/app_rpt/rpt_vox.h"
+rpt_add "apps/app_rpt/rpt_uchameleon.c"
+rpt_add "apps/app_rpt/rpt_uchameleon.h"
+rpt_add "apps/app_rpt/rpt_bridging.c"
+rpt_add "apps/app_rpt/rpt_bridging.h"
+rpt_add "apps/app_rpt/rpt_radio.c"
+rpt_add "apps/app_rpt/rpt_radio.h"
+rpt_add "apps/app_rpt/rpt_channel.c"
+rpt_add "apps/app_rpt/rpt_channel.h"
+rpt_add "apps/app_rpt/rpt_config.c"
+rpt_add "apps/app_rpt/rpt_config.h"
+rpt_add "apps/app_rpt/rpt_link.c"
+rpt_add "apps/app_rpt/rpt_link.h"
+rpt_add "apps/app_rpt/rpt_functions.c"
+rpt_add "apps/app_rpt/rpt_functions.h"
+rpt_add "apps/app_rpt/rpt_telemetry.c"
+rpt_add "apps/app_rpt/rpt_telemetry.h"
+rpt_add "apps/app_rpt/rpt_manager.c"
+rpt_add "apps/app_rpt/rpt_manager.h"
+rpt_add "apps/app_rpt/rpt_translate.c"
+rpt_add "apps/app_rpt/rpt_translate.h"
+rpt_add "apps/app_rpt/rpt_rig.c"
+rpt_add "apps/app_rpt/rpt_rig.h"
+rpt_add "channels/chan_beagle.c"
+rpt_add "channels/chan_echolink.c"
+rpt_add "channels/chan_simpleusb.c"
+rpt_add "channels/chan_usbradio.c"
+rpt_add "channels/chan_tlb.c"
+rpt_add "channels/chan_voter.c"
+rpt_add "channels/chan_usrp.c"
+rpt_add "channels/chan_usrp.h"
+rpt_add "channels/xpmr/sinetabx.h"
+rpt_add "channels/xpmr/xpmr.c"
+rpt_add "channels/xpmr/xpmr.h"
+rpt_add "channels/xpmr/xpmr_coef.h"
+rpt_add "configs/samples/rpt_http_registrations.conf.sample"
+rpt_add "configs/samples/usbradio.conf.sample"
+rpt_add "configs/samples/simpleusb.conf.sample"
+rpt_add "include/asterisk/res_usbradio.h"
+rpt_add "res/res_rpt_http_registrations.c"
+rpt_add "res/res_usbradio.c"
+rpt_add "res/res_usbradio.exports.in"
+>>>>>>> 594948c (chan_beagle: Initial port to compile on current Asterisk.)
 
 make -j$(nproc) apps && make -j$(nproc) channels && make -j$(nproc) res && make install
 if [ $? -ne 0 ]; then
