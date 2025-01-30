a10863ecc59de9aac3cd8159ac4f30fa9cfe6996
mkmer <m_kasper@sbcglobal.net>
2025-01-30 10:06:48 -0500

    rpt_telemetry.c: Update explode_string() to ARRAY_LEN

--- apps/app_rpt/rpt_telemetry.c
+++ apps/app_rpt/rpt_telemetry.c
@@ -92,7 +92,7 @@ int handle_meter_tele(struct rpt *myrpt, struct ast_channel *mychannel, char *ar
 		return -1;
 	}
 
-	i = explode_string(myargs, argv, 4, ',', 0);
+	i = explode_string(myargs, argv, ARRAY_LEN(argv), ',', 0);
 	if ((i != 4) && (i != 3)) {	/* Must have 3 or 4 substrings, no more, no less */
 		ast_log(LOG_WARNING, "Wrong number of arguments for meter telemetry function is: %d s/b 3 or 4", i);
 		ast_free(myargs);
@@ -196,7 +196,7 @@ int handle_meter_tele(struct rpt *myrpt, struct ast_channel *mychannel, char *ar
 		/*
 		 * Parse range entries
 		 */
-		if ((numranges = explode_string(start, range_strings, MAX_DAQ_RANGES, ',', 0)) < 2) {
+		if ((numranges = explode_string(start, range_strings, ARRAY_LEN(range_strings), ',', 0)) < 2) {
 			ast_log(LOG_WARNING, "At least 2 ranges required for range() in meter face %s\n", argv[2]);
 			ast_free(myargs);
 			ast_free(meter_face);
@@ -214,7 +214,7 @@ int handle_meter_tele(struct rpt *myrpt, struct ast_channel *mychannel, char *ar
 		*start++ = 0;
 		*end = 0;
 		sounds = end + 2;
-		if (2 != explode_string(start, bitphrases, 2, ',', 0)) {
+		if (2 != explode_string(start, bitphrases, ARRAY_LEN(bitphrases), ',', 0)) {
 			ast_log(LOG_WARNING, "2 phrases required for bit() in meter face %s\n", argv[2]);
 			ast_free(myargs);
 			ast_free(meter_face);
@@ -292,7 +292,7 @@ int handle_meter_tele(struct rpt *myrpt, struct ast_channel *mychannel, char *ar
 	}
 
 	/* Split up the sounds string */
-	files = explode_string(sounds, sound_files, MAX_METER_FILES, ',', 0);
+	files = explode_string(sounds, sound_files, ARRAY_LEN(sound_files), ',', 0);
 	if (files == 0) {
 		ast_log(LOG_WARNING, "No sound files to say for meter %s\n", argv[2]);
 		ast_free(myargs);
