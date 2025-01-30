af9fd9b2582e088c18bd9067ec2980a8a7495703
mkmer <m_kasper@sbcglobal.net>
2025-01-30 09:08:02 -0500

    rpt_manager.c ARRAY_LEN()

--- apps/app_rpt/rpt_manager.c
+++ apps/app_rpt/rpt_manager.c
@@ -271,7 +271,7 @@ static int rpt_manager_do_xstat(struct mansession *ses, const struct message *m,
 			/* Get all linked nodes info */
 
 			/* parse em */
-			ns = finddelim(lbuf, strs, sizeof(strs));
+			ns = finddelim(lbuf, strs, ARRAY_LEN(strs));
 			/* sort em */
 			if (ns) {
 				qsort((void *) strs, ns, sizeof(char *), mycompar);
