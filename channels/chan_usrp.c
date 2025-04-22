1c7f21dc0491f184512e65a58d3364fab96a8d34
InterLinked1 <24227567+InterLinked1@users.noreply.github.com>
2025-04-22 09:42:26 -0400

    chan_usrp: Get rid of chan_usrp.h. (#641)
    Consolidate the contents of chan_usrp.h into chan_usrp.c since there are no other users of it.

--- channels/chan_usrp.c
+++ channels/chan_usrp.c
@@ -73,13 +73,31 @@
 #include "asterisk/dsp.h"
 #include "asterisk/format_cache.h"
 
-#include "chan_usrp.h"
-
 #define	MAX_RXKEY_TIME 4
 #define	KEEPALIVE_TIME 50 * 7
 #define	BLOCKING_FACTOR 4
 #define	SSO sizeof(unsigned long)
 #define QUEUE_OVERLOAD_THRESHOLD 25
+
+#define USRP_VOICE_FRAME_SIZE (160 * sizeof(short)) /* 0.02 * 8k */
+
+enum {
+	USRP_TYPE_VOICE = 0,
+	USRP_TYPE_DTMF,
+	USRP_TYPE_TEXT
+};
+
+/*! \brief UDP data header */
+struct _chan_usrp_bufhdr {
+	char eye[4];		/*!< verification string */
+	uint32_t seq;		/*!< sequence counter */
+	uint32_t memory;	/*!< memory ID or zero (default) */
+	uint32_t keyup;		/*!< tracks PTT state */
+	uint32_t talkgroup;	/*!< trunk TG id */
+	uint32_t type;		/*!< see above enum */
+	uint32_t mpxid;		/*!< for future use */
+	uint32_t reserved;	/*!< for future use */
+};
 
 static const char tdesc[] = "USRP Driver";
 
