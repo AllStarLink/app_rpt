341fe8fe6eb9098b9a1481d68425c5e788790932
Allan Nathanson <42244061+Allan-N@users.noreply.github.com>
2025-04-30 09:51:32 -0400

    Bump app_rpt version (to 3.4.5)

--- apps/app_rpt/app_rpt.h
+++ apps/app_rpt/app_rpt.h
@@ -1,6 +1,6 @@
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 4
-#define VERSION_PATCH 4
+#define VERSION_PATCH 5
 
 /* 99% of the DSP code in app_rpt exists in dsp.c as private functions. This code can mostly be
 	converted to use public dsp.h API.
