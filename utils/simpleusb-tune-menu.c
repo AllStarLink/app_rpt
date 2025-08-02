a46026ab7061ab7f9eefe6d8280a342bcfde075b
Allan Nathanson <42244061+Allan-N@users.noreply.github.com>
2025-08-02 11:27:05 -0400

    *-tune-menu: (S)wap command should not exit when no alternate devices (#730)

--- utils/simpleusb-tune-menu.c
+++ utils/simpleusb-tune-menu.c
@@ -417,12 +417,12 @@ static int astgetresp(char *cmd)
 	}
 
 	/* get device list from Asterisk */
-	if (astgetline(COMMAND_PREFIX "tune menu-support 1", buf, sizeof(buf) - 1)) {
+	if (astgetline(COMMAND_PREFIX "tune menu-support 1", buf, sizeof(buf) - 1) < 0) {
 		exit(255);
 	}
 	n = explode_string(buf, strs, ARRAY_LEN(strs), ',', 0);
 	if (n < 1) {
-		fprintf(stderr, "Error parsing USB device information\n");
+		fprintf(stderr, "No USB devices found\n");
 		return;
 	}
 	qsort(strs, n, sizeof(char *), qcompar);
@@ -475,11 +475,11 @@ static int astgetresp(char *cmd)
 	}
 
 	/* get device list from Asterisk */
-	if (astgetline(COMMAND_PREFIX "tune menu-support 3", buf, sizeof(buf) - 1)) {
+	if (astgetline(COMMAND_PREFIX "tune menu-support 3", buf, sizeof(buf) - 1) < 0) {
 		exit(255);
 	}
 	n = explode_string(buf, strs, ARRAY_LEN(strs), ',', 0);
-	if ((n < 1) || (!*strs[0])) {
+	if (n < 1) {
 		fprintf(stderr, "No additional USB devices found\n");
 		return;
 	}
