bf2fd68888d175e1574f3d0b697dc5c896d87b4b
Steve Rodgers <hwstar@users.noreply.github.com>
2025-08-22 07:57:07 -0700

    Update apps/app_rpt.c
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- apps/app_rpt.c
+++ apps/app_rpt.c
@@ -5175,7 +5175,7 @@ static void *rpt(void *this)
 		if (myrpt->p.duplex > 0) {
 			/* If timed out, we only want to keep the TX keyed if there
 			 * is a message queued which is configured to override
-			 *  the time out condition 
+			 *  the time out condition
 			 */
 			if ((!myrpt->totimer) || myrpt->tounkeyed)  {
 				totx = totx || tot_override_message_pending(myrpt);
