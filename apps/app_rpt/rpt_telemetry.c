06f40b8245cc1fd1a05807c172dc8e8d515d6b1b
mkmer <m_kasper@sbcglobal.net>
2025-11-13 07:48:20 -0500

    Missing locks

--- apps/app_rpt/rpt_telemetry.c
+++ apps/app_rpt/rpt_telemetry.c
@@ -1556,6 +1556,7 @@ void *rpt_tele_thread(void *this)
 treataslocal:
 		lbuf = ast_str_create(RPT_AST_STR_INIT_SIZE);
 		if (!lbuf) {
+			rpt_mutex_lock(&myrpt->lock);
 			goto abort;
 		}
 		rpt_mutex_lock(&myrpt->lock);
@@ -1565,6 +1566,7 @@ treataslocal:
 		strs = ast_malloc(n * sizeof(char *));
 		if (!strs) {
 			ast_free(lbuf);
+			rpt_mutex_lock(&myrpt->lock);
 			goto abort;
 		}
 		/* parse em */
@@ -2358,12 +2360,12 @@ treataslocal:
 			NULL,								 /* Sorting function. NULL means the list will not be sorted */
 			NULL);								 /* Comparison function */
 		if (!links_copy) {
+			rpt_mutex_lock(&myrpt->lock);
 			goto abort;
 		}
 		rpt_mutex_lock(&myrpt->lock);
 		/* make our own list of links */
 		if (ao2_container_dup(links_copy, myrpt->links, OBJ_NOLOCK)) {
-			rpt_mutex_unlock(&myrpt->lock);
 			ao2_cleanup(links_copy);
 			goto abort;
 		}
@@ -2421,6 +2423,7 @@ treataslocal:
 	case FULLSTATUS:
 		lbuf = ast_str_create(RPT_AST_STR_INIT_SIZE);
 		if (!lbuf) {
+			rpt_mutex_lock(&myrpt->lock);
 			goto abort;
 		}
 		rpt_mutex_lock(&myrpt->lock);
@@ -2430,6 +2433,7 @@ treataslocal:
 		strs = ast_malloc(n * sizeof(char *));
 		if (!strs) {
 			ast_free(lbuf);
+			rpt_mutex_lock(&myrpt->lock);
 			goto abort;
 		}
 		/* parse em */
