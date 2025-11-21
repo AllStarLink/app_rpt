c2346b1e8383e094df3f920d4e8f314c6e60d6e8
mkmer <m_kasper@sbcglobal.net>
2025-11-21 09:17:15 -0500

    Refactor new if statement to include `!p` test

--- channels/chan_voter.c
+++ channels/chan_voter.c
@@ -3926,12 +3926,12 @@ static void *voter_reader(void *data)
 						break;
 					}
 				}
-			}
-			if (!p) {
-				/* We didn't find an asterisk channel,
-				 * act like we don't know the client.
-				 */
-				client = NULL;
+				if (!p) {
+					/* We didn't find an asterisk channel,
+					 * act like we don't know the client.
+					 */
+					client = NULL;
+				}
 			}
 			if (client) {
 				if (check_client_sanity && p && !p->priconn) {
