a7cf7bc9a51bb59c106e1b06806eb9a820d1f827
Mike Kasper <mike.j.kasper@gmail.com>
2025-12-31 14:00:42 -0500

    Remove stale comments

--- apps/app_rpt/rpt_telemetry.c
+++ apps/app_rpt/rpt_telemetry.c
@@ -1314,7 +1314,7 @@ void *rpt_tele_thread(void *this)
 	if (!cap) {
 		ast_log(LOG_ERROR, "Failed to alloc cap\n");
 		rpt_mutex_lock(&myrpt->lock);
-		goto abort; /* Didn't set active_telem, so goto abort2, not abort. */
+		goto abort;
 	}
 
 	ast_format_cap_append(cap, ast_format_slin, 0);
@@ -1325,7 +1325,7 @@ void *rpt_tele_thread(void *this)
 	if (!mychannel) {
 		ast_log(LOG_WARNING, "Unable to obtain local channel (mode: %d)\n", mytele->mode);
 		rpt_mutex_lock(&myrpt->lock);
-		goto abort; /* Didn't set active_telem, so goto abort2, not abort. */
+		goto abort;
 	}
 	ast_debug(1, "Requested channel %s\n", ast_channel_name(mychannel));
 	ast_channel_ref(mychannel); /* Create a reference to prevent channel from being freed too soon */
