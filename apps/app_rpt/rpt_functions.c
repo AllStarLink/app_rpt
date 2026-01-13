7d2fc50bd927642e60cf3c353c153ab527398255
Mike Kasper <mike.j.kasper@gmail.com>
2026-01-13 07:45:59 -0500

    Refactor ilink commands

--- apps/app_rpt/rpt_functions.c
+++ apps/app_rpt/rpt_functions.c
@@ -88,7 +88,6 @@ enum rpt_function_response function_ilink(struct rpt *myrpt, char *param, char *
 	int i, r;
 	struct rpt_connect_data *connect_data;
 	pthread_t connect_threadid;
-	struct ast_frame wf;
 
 	if (!param)
 		return DC_ERROR;
@@ -101,7 +100,9 @@ enum rpt_function_response function_ilink(struct rpt *myrpt, char *param, char *
 	ast_debug(7, "@@@@ ilink param = %s, digitbuf = %s\n", (param) ? param : "(null)", digitbuf);
 	switch (myatoi(param)) {
 	case 11:					/* Perm Link off */
-	case 1:						/* Link off */
+	case 1: {					/* Link off */
+		struct ast_frame wf;
+
 		if (strlen(digitbuf) < 1)
 			break;
 		if ((digitbuf[0] == '0') && (myrpt->lastlinknode[0]))
@@ -137,6 +138,7 @@ enum rpt_function_response function_ilink(struct rpt *myrpt, char *param, char *
 		rpt_telemetry(myrpt, COMPLETE, NULL);
 		ao2_ref(l, -1);
 		return DC_COMPLETE;
+	}
 	case 2:					/* Link Monitor */
 	case 3:					/* Link transceive */
 	case 12:					/* Link Monitor permanent */
@@ -233,12 +235,18 @@ enum rpt_function_response function_ilink(struct rpt *myrpt, char *param, char *
 		rpt_telemetry(myrpt, FULLSTATUS, NULL);
 		return DC_COMPLETE;
 
-	case 6:					/* All Links Off, including permalinks */
+	case 6: { /* All Links Off, including permalinks */
+		struct ast_frame wf;
+		init_text_frame(&wf, "function_ilink:6");
+		wf.datalen = strlen(DISCSTR) + 1;
+		wf.data.ptr = DISCSTR;
+
 		rpt_mutex_lock(&myrpt->lock);
 		myrpt->savednodes[0] = 0;
 		/* loop through all links */
 		RPT_LIST_TRAVERSE(myrpt->links, l, l_it) {
 			char c1;
+
 			if ((l->name[0] <= '0') || (l->name[0] > '9')) { /* Skip any IAXRPT monitoring */
 				continue;
 			}
@@ -258,10 +266,7 @@ enum rpt_function_response function_ilink(struct rpt *myrpt, char *param, char *
 			l->retries = l->max_retries + 1;
 			l->disced = RPT_LINK_DISCONNECT_SILENT; /* Silently disconnect */
 			rpt_mutex_unlock(&myrpt->lock);
-			ast_debug(5, "dumping link %s\n",l->name);
-			init_text_frame(&wf, "function_ilink:6");
-			wf.datalen = strlen(DISCSTR) + 1;
-			wf.data.ptr = DISCSTR;
+			ast_debug(5, "dumping link %s\n", l->name);
 			if (l->chan) {
 				if (l->thisconnected) {
 					rpt_qwrite(l, &wf);
@@ -275,7 +280,7 @@ enum rpt_function_response function_ilink(struct rpt *myrpt, char *param, char *
 		rpt_telem_select(myrpt, command_source, mylink);
 		rpt_telemetry(myrpt, COMPLETE, NULL);
 		return DC_COMPLETE;
-
+	}
 	case 7:					/* Identify last node which keyed us up */
 		rpt_telem_select(myrpt, command_source, mylink);
 		rpt_telemetry(myrpt, LASTNODEKEY, NULL);
