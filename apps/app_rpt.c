AllStarLink/app_rpt

commit 6c883d2e6fc1800f52d6e49efc0982d6063f77b7

app_rpt: limit text message size, adjust stat post and link update interval (#933)
It appears that fragmented IAX frames can cause network storms.  To help, we are adding a new configuration variable that will truncate the size of text messages.

- Add "linkpost_max_message_len=" variable to specify the max length of an "L" message.
  - default value is 0 (no truncation), min 500 bytes, max 10k bytes

In addition, we've been posting updates to "stats.allstarlink.org" every 30 seconds and the IAX "L" (link update) messages every 10 seconds.  Both of these intervals can/should be throttled back.

- Add "statpost_time=" variable to specify how often we post link information to "stats.allstarlink.org".
  - past interval was 30s
  - new default value is 60s, min 30s, max 10m

- Add "linkpost_time=" variable to specify how often we send "L" messages to connected nodes.
  - past interval was 10s
  - new default value is 60s, min 10s, max 10m

Lastly, the do_link_post() code has been updated to use __mklinklist()

apps/app_rpt.c
@@ -3305,7 +3305,7 @@ static inline void periodic_process_link(struct rpt *myrpt, struct rpt_link *l, 
 			return;
 		}
 		init_text_frame(&lf, __PRETTY_FUNCTION__);
-		l->linklisttimer = LINKLISTTIME;
+		l->linklisttimer = myrpt->p.linkpost_time * 1000;
 		ast_str_set(&lstr, 0, "%s", "L ");
 		rpt_mutex_lock(&myrpt->lock);
 		__mklinklist(myrpt, l, &lstr, 0);
@@ -3449,39 +3449,21 @@ static inline void do_key_post(struct rpt *myrpt)
  */
 static inline int do_link_post(struct rpt *myrpt)
 {
-	int nstr;
-	char lst;
 	struct ast_str *str;
 	time_t now;
-	struct rpt_link *l;
-	struct ao2_iterator l_it;
 
-	myrpt->linkposttimer = LINKPOSTTIME;
+	if (!myrpt->p.statpost_url) {
+		return 0;
+	}
+
+	myrpt->linkposttimer = myrpt->p.statpost_time * 1000;
 
 	str = ast_str_create(RPT_AST_STR_INIT_SIZE);
 	if (!str) {
 		return -1;
 	}
-	nstr = 0;
 	ast_str_set(&str, 0, "%s", "&nodes=");
-	RPT_LIST_TRAVERSE(myrpt->links, l, l_it) {
-		/* if is not a real link, ignore it */
-		if (l->name[0] == '0') {
-			continue;
-		}
-		lst = 'T';
-		if (l->mode == MODE_MONITOR)
-			lst = 'R';
-		if (l->mode == MODE_LOCAL_MONITOR)
-			lst = 'L';
-		if (!l->thisconnected)
-			lst = 'C';
-		if (nstr)
-			ast_str_append(&str, 0, "%s", ",");
-		ast_str_append(&str, 0, "%c%s", lst, l->name);
-		nstr = 1;
-	}
-	ao2_iterator_destroy(&l_it);
+	__mklinklist(myrpt, NULL, &str, USE_FORMAT_RPT_LINKPOST);
 	time(&now);
 
 	ast_str_append(&str, 0,

apps/app_rpt/rpt_link.c
@@ -393,85 +393,213 @@ void rpt_link_remove(struct ao2_container *links, struct rpt_link *l)
 	ao2_unlink(links, l);
 }
 
-int __mklinklist(struct rpt *myrpt, struct rpt_link *mylink, struct ast_str **buf, int alink_format) {
+static int __mklinklist_limit(struct rpt *myrpt, struct ast_str *buf, int bytes, enum __mklinklist_flags flags)
+{
+	int new_len;
+
+	if (!(flags & LIMIT_STRING_LENGTH)) {
+		/* we are not limiting the length */
+		return 0;
+	}
+
+	if (myrpt->p.linkpost_max_message_len == 0) {
+		/* we are not truncating the message size */
+		return 0;
+	}
+
+	if (bytes == 0) {
+		/* we are not appending any characters */
+		return 0;
+	}
+
+	new_len = ast_str_strlen(buf) + bytes;
+	if (new_len > myrpt->p.linkpost_max_message_len) {
+		/* if adding the [string for the] next node might lead to a fragmented IAX2 message frame */
+		return 1;
+	}
+
+	return 0;
+}
+
+static void __mklinklist_mode_adjust(char *cp, int mode)
+{
+	if (*cp == 'T') {
+		*cp = mode;
+	}
+	if ((*cp == 'R') && (mode == 'C')) {
+		*cp = mode;
+	}
+}
+
+int __mklinklist(struct rpt *myrpt, struct rpt_link *mylink, struct ast_str **buf, enum __mklinklist_flags flags)
+{
 	struct rpt_link *l;
 	struct ao2_iterator l_it;
-	char mode, *links_buf;
-	int i, spos, len, links_count = 0, one_link = 0;
-	if (myrpt->remote)
+	const char *sep = "";
+	char mode, *str;
+	int i, spos, len, links_count = 0;
+
+	if (myrpt->remote) {
 		return 0;
+	}
+
+	spos = ast_str_strlen(*buf); /* current buf size (before we add our stuff) */
+
 	/* go thru all links */
 	RPT_LIST_TRAVERSE(myrpt->links, l, l_it) {
-		/* if is not a real link, ignore it */
 		if (l->name[0] == '0') {
+			/* if is not a real link, ignore it */
 			continue;
 		}
-		if (l->mode == MODE_LOCAL_MONITOR) {
-			continue; /* dont report local modes */
+		if (!(flags & USE_FORMAT_RPT_LINKPOST)) {
+			if (l->mode == MODE_LOCAL_MONITOR) {
+				/* dont report local nodes */
+				continue;
+			}
+			if (l == mylink) {
+				/* dont count our stuff */
+				continue;
+			}
+			if (mylink && !strcmp(l->name, mylink->name)) {
+				continue;
+			}
 		}
-		/* dont count our stuff */
-		if (l == mylink) {
-			continue;
-		}
-		if (mylink && !strcmp(l->name, mylink->name)) {
-			continue;
-		}
-		/* figure out mode to report */
+
+		/* figure out "mode" to report */
 		mode = 'T'; /* use Transceive by default */
 		if (l->mode == MODE_MONITOR) {
 			mode = 'R'; /* indicate RX for our mode */
+		} else if (l->mode == MODE_LOCAL_MONITOR) {
+			mode = 'L'; /* indicate RX for our mode */
 		}
 		if (!l->thisconnected) {
 			mode = 'C'; /* indicate connecting */
 		}
-		spos = ast_str_strlen(*buf); /* current buf size (b4 we add our stuff) */
-		if (spos > 2) {
-			ast_str_append(buf, 0, "%s", ",");
+
+		len = strlen(l->name);
+		if (links_count++ > 0) {
+			sep = ",";
+			len++;
 		}
-	    one_link = 1;
-		if (alink_format) { /* RPT_ALINK format - only show adjacent nodes*/
-			ast_str_append(buf, 0, "%s%c%c", l->name, mode, (l->lastrx1) ? 'K' : 'U');
-		} else { /* RPT_LINK format - show all nodes*/
-			/* add nodes into buffer */
-			if (ast_str_strlen(l->linklist)) {
-				ast_str_append(buf, 0, "%c%s,%s", mode, l->name, ast_str_buffer(l->linklist));
-			} else { /* if no nodes, add this node into buffer */
-				ast_str_append(buf, 0, "%c%s", mode, l->name);
+
+		if (flags & USE_FORMAT_RPT_ALINK) {
+			/*
+			 * RPT_ALINK format
+			 * - show only adjacent nodes
+			 * - for each node, include name, mode, and last keyed status
+			 */
+			if (__mklinklist_limit(myrpt, *buf, len + 2 + sizeof("000000RU"), flags)) {
+				/* if adding the name, mode, lastrx, and separator will result in fragmentation */
+				ast_str_append(buf, 0, "%s%s%c%c", sep, "000000", 'R', 'U');
+				break;
+			}
+
+			/* add the adjacent node */
+			ast_str_append(buf, 0, "%s%s%c%c", sep, l->name, mode, l->lastrx1 ? 'K' : 'U');
+		} else {
+			/*
+			 * RPT_LINK format
+			 * - show all nodes (including those linked)
+			 * - for each node, include mode and name
+			 */
+			if (__mklinklist_limit(myrpt, *buf, len + 1 + sizeof("R000000"), flags)) {
+				/* if adding the name, mode, lastrx, and separator will result in fragmentation */
+				ast_str_append(buf, 0, "%s%c%s", sep, 'R', "000000");
+				break;
+			}
+
+			/* add the adjacent node */
+			ast_str_append(buf, 0, "%s%c%s", sep, mode, l->name);
+
+			/* and append any nodes linked to the adjacent node */
+			str = ast_str_buffer(l->linklist);
+			len = ast_str_strlen(l->linklist);
+			if (len > 0) {
+				int truncated = 0;
+
+				/*
+				 * Check to see if all of the associated / linked nodes can be
+				 * appended to the string.  If not all will fit, chop off those
+				 * nodes that will not result in fragmentation.
+				 */
+				while ((len > 0) && __mklinklist_limit(myrpt, *buf, len + sizeof("R000000"), flags)) {
+					truncated = 1;
+
+					while ((len > 0) && (str[len - 1] != ',')) {
+						--len; /* remove characters of the last link */
+					}
+					if (len > 0) {
+						--len; /* and remove the separator */
+					}
+				}
+
+				if (len > 0) {
+					ast_str_append(buf, 0, ",%.*s", len, str);
+
+					links_count++; /* include the 1st linked node in the count */
+					for (i = 0; i < len; i++) {
+						if (str[i] == ',') {
+							links_count++; /* include the 2nd, 3rd, ... in the count */
+						}
+					}
+				}
+
+				if (truncated) {
+					ast_str_append(buf, 0, ",%c%s", 'R', "000000");
+					break;
+				}
 			}
 		}
-		/* if we are in transceive mode, let all modes stand */
+
+		if (flags & USE_FORMAT_RPT_LINKPOST) {
+			continue;
+		}
+
 		if (mode == 'T') {
 			continue;
 		}
-		/* downgrade everyone on this node if appropriate */
-		links_buf = ast_str_buffer(*buf);
+
+		/* if this node is not in transceive mode, downgrade everyone on this node if appropriate */
+		str = ast_str_buffer(*buf);
 		len = ast_str_strlen(*buf);
-		for (i = spos; i < len; i++) {
-			if (links_buf[i] == 'T') {
-				links_buf[i] = mode;
+		if (flags & USE_FORMAT_RPT_ALINK) {
+			/*
+			 * RPT_ALINK format
+			 *   L <name><mode><keyed>,<name><mode><keyed>
+			 *           ^^^^^^              ^^^^^^
+			 */
+			for (i = len - 1; i >= spos + 2; --i) {
+				/* update <mode> (<mode> is at end of token, just before <keyed>) */
+				__mklinklist_mode_adjust(&str[i - 1], mode);
+
+				/* skip back past <mode><keyed> and then to the previous ',' */
+				i -= 2;
+				while (i >= spos && str[i] != ',') {
+					--i;
+				}
 			}
-			if ((links_buf[i] == 'R') && (mode == 'C')) {
-				links_buf[i] = mode;
+		} else {
+			/*
+			 * RPT_LINK format
+			 *   L <mode><name>,<mode><name>
+			 *     ^^^^^^       ^^^^^^
+			 */
+			for (i = spos; i < len; ++i) {
+				/* update <mode> */
+				__mklinklist_mode_adjust(&str[i], mode);
+
+				/* skip past the <mode> and then to the next "," */
+				while ((i < len) && (str[i] != ',')) {
+					++i;
+				}
 			}
 		}
 	}
 	ao2_iterator_destroy(&l_it);
-	/* After building the string, count number of nodes (commas) in buffer string. The first
-	 * node doesn't have a comma, so we need to add 1 if there is at least one_link.
-	 */
-	links_count = 0;
-	links_buf = ast_str_buffer(*buf);
-	for (i = 0; i < ast_str_strlen(*buf); i++) {
-		if (links_buf[i] == ',') {
-			links_count++;
-		}
-	}
-	if (one_link) { /* The first link in the list has no comma but we have 1 link */
-		links_count++;
-	}
 
 	return links_count;
 }
+
 static int link_set_list_timer_cb(void *obj, void *arg, int flags)
 {
 	struct rpt_link *link = obj;
@@ -512,7 +640,7 @@ void rpt_update_links(struct rpt *myrpt)
 	}
 
 	rpt_mutex_lock(&myrpt->lock);
-	n = __mklinklist(myrpt, NULL, &buf, 1);
+	n = __mklinklist(myrpt, NULL, &buf, USE_FORMAT_RPT_ALINK);
 	rpt_mutex_unlock(&myrpt->lock);
 	/* parse em */
 	if (n) {
@@ -526,7 +654,7 @@ void rpt_update_links(struct rpt *myrpt)
 
 	ast_str_reset(buf);
 	rpt_mutex_lock(&myrpt->lock);
-	n = __mklinklist(myrpt, NULL, &buf, 0);
+	n = __mklinklist(myrpt, NULL, &buf, USE_FORMAT_RPT_LINK);
 	rpt_mutex_unlock(&myrpt->lock);
 	if (n) {
 		ast_str_set(&obuf, 0, "%d,%s", n, ast_str_buffer(buf));
@@ -658,7 +786,7 @@ void *rpt_link_connect(void *data)
 			rpt_mutex_unlock(&myrpt->lock);
 			goto cleanup;
 		}
-		n = __mklinklist(myrpt, NULL, &lstr, 0) + 1;
+		n = __mklinklist(myrpt, NULL, &lstr, USE_FORMAT_RPT_LINK) + 1;
 		rpt_mutex_unlock(&myrpt->lock);
 		strs = ast_malloc(n * sizeof(char *));
 		if (!strs) {

apps/app_rpt/app_rpt.h
@@ -74,9 +74,8 @@ typedef struct {
 #define	MAXMACRO 2048
 #define	MAXNODES 500			  /* Maximum number of nodes allowed in the link list */
 #define	RPT_AST_STR_INIT_SIZE 500 /* initial guess for ast_str size */
-#define	LINKLISTTIME 10000
-#define	LINKLISTSHORTTIME 200
-#define	LINKPOSTTIME 30000
+
+#define LINKLISTSHORTTIME 200
 #define LINKPOSTSHORTTIME 200
 #define	KEYTIMERTIME 250
 #define	MACROTIME 100
@@ -206,8 +205,6 @@ typedef struct {
 #define	SIMPLEX_PHONE_DELAY 25
 
 #define RX_LINGER_TIME 50
-
-#define	STATPOST_PROGRAM "/usr/bin/wget,-q,--output-document=/dev/null,--no-check-certificate"
 
 #define	ALLOW_LOCAL_CHANNELS
 
@@ -863,8 +860,10 @@ struct rpt {
 		int simplexpatchdelay;
 		int simplexphonedelay;
 		char telemdefault;
-		const char *statpost_program;
+		int linkpost_time;
+		int linkpost_max_message_len;
 		const char *statpost_url;
+		int statpost_time;
 		enum rpt_linkmode linkmode[10];
 		char linkmodedynamic[10];
 		const char *locallist[16];

apps/app_rpt/rpt_cli.c
@@ -525,7 +525,7 @@ static int rpt_do_xnode(int fd, int argc, const char *const *argv)
 			/* ### GET CONNECTED NODE INFO ####################
 			 * Traverse the list of connected nodes
 			 */
-			n = __mklinklist(myrpt, NULL, &lbuf, 0) + 1;
+			n = __mklinklist(myrpt, NULL, &lbuf, USE_FORMAT_RPT_LINK) + 1;
 
 			j = 0;
 			rpt_mutex_unlock(&myrpt->lock); // UNLOCK
@@ -646,7 +646,7 @@ static int rpt_do_nodes(int fd, int argc, const char *const *argv)
 			/* Make a copy of all stat variables while locked */
 			myrpt = &rpt_vars[i];
 			rpt_mutex_lock(&myrpt->lock);	/* LOCK */
-			n = __mklinklist(myrpt, NULL, &lbuf, 0) + 1;
+			n = __mklinklist(myrpt, NULL, &lbuf, USE_FORMAT_RPT_LINK) + 1;
 			rpt_mutex_unlock(&myrpt->lock);	/* UNLOCK */
 			strs = ast_malloc(n * sizeof(char *));
 			if (!strs) {

apps/app_rpt/rpt_config.c
@@ -877,7 +877,17 @@ void load_rpt_vars(int n, int init)
 	RPT_CONFIG_VAR_INT_DEFAULT(voxrecover_ms, "voxrecover", VOX_RECOVER_MS);
 	RPT_CONFIG_VAR_INT_DEFAULT(simplexpatchdelay, "simplexpatchdelay", SIMPLEX_PATCH_DELAY);
 	RPT_CONFIG_VAR_INT_DEFAULT(simplexphonedelay, "simplexphonedelay", SIMPLEX_PHONE_DELAY);
-	RPT_CONFIG_VAR_DEFAULT(statpost_program, "statpost_program", STATPOST_PROGRAM);
+
+	/* configure how "L" messages are sent */
+	RPT_CONFIG_VAR_INT_DEFAULT_MIN_MAX(linkpost_max_message_len, "linkpost_max_message_len", 0, 0, 10000);
+	if (rpt_vars[n].p.linkpost_max_message_len && (rpt_vars[n].p.linkpost_max_message_len < 500)) {
+		/* if message truncation enabled, set minimum */
+		rpt_vars[n].p.linkpost_max_message_len = 500;
+	}
+	RPT_CONFIG_VAR_INT_DEFAULT_MIN_MAX(linkpost_time, "linkpost_time", 60, 10, 600);
+
+	/* configure how we interact with "stats.allstarlink.org" */
+	RPT_CONFIG_VAR_INT_DEFAULT_MIN_MAX(statpost_time, "statpost_time", 60, 30, 600);
 	RPT_CONFIG_VAR(statpost_url, "statpost_url");
 
 	rpt_vars[n].p.tailmessagetime = retrieve_astcfgint(&rpt_vars[n], cat, "tailmessagetime", 0, 200000000, 0);

apps/app_rpt/rpt_link.h
@@ -64,16 +64,30 @@ void rpt_link_remove(struct ao2_container *links, struct rpt_link *l);
 void rpt_link_destroy(void *obj);
 
 /*!
+ * \brief __mklinklist() flags
+ */
+enum __mklinklist_flags {
+	/*! \brief Create RPT_LINK format (<mode><node>) string */
+	USE_FORMAT_RPT_LINK = 0,
+	/*! \brief Create RPT_ALINK format (<node><mode><keystate>) string */
+	USE_FORMAT_RPT_ALINK = (1 << 0),
+	/*! \brief Create RPT_LINKPOST format (<mode><node>) string */
+	USE_FORMAT_RPT_LINKPOST = (1 << 1),
+	/*! \brief Limit string length to avoid fragmentation */
+	LIMIT_STRING_LENGTH = (1 << 8),
+};
+
+/*!
  * \brief Create a list of links for this node.
  * Must be called locked.
  * \param myrpt		Pointer to rpt structure.
  * \param mylink	Pointer to rpt_link structure.
  * \param buf		Pointer to ast_str buffer - link string is generated in this buffer.
- * \param alink_format	Flag to indicate if RPT_ALINK format is returned. If not, RPT_LINK
+ * \param flags		Flags specifying format as the returned string.
  * format is returned. \retval		link count.
  */
 
-int __mklinklist(struct rpt *myrpt, struct rpt_link *mylink, struct ast_str **buf, int alink_format);
+int __mklinklist(struct rpt *myrpt, struct rpt_link *mylink, struct ast_str **buf, enum __mklinklist_flags flags);
 
 /*! \brief must be called locked */
 void __kickshort(struct rpt *myrpt);

apps/app_rpt/rpt_manager.c
@@ -208,7 +208,7 @@ static int rpt_manager_do_xstat(struct mansession *ses, const struct message *m)
 
 			/* Get connected node info */
 			/* Traverse the list of connected nodes */
-			n = __mklinklist(myrpt, NULL, &lbuf, 0) + 1;
+			n = __mklinklist(myrpt, NULL, &lbuf, USE_FORMAT_RPT_LINK) + 1;
 			links_copy = ao2_container_clone(myrpt->links, OBJ_NOLOCK);
 			rpt_mutex_unlock(&myrpt->lock);
 			if (!links_copy) {

apps/app_rpt/rpt_telemetry.c
@@ -1575,7 +1575,7 @@ treataslocal:
 			goto abort;
 		}
 		/* get all the nodes */
-		n = __mklinklist(myrpt, NULL, &lbuf, 0) + 1;
+		n = __mklinklist(myrpt, NULL, &lbuf, USE_FORMAT_RPT_LINK) + 1;
 		rpt_mutex_unlock(&myrpt->lock);
 		strs = ast_malloc(n * sizeof(char *));
 		if (!strs) {
@@ -2414,7 +2414,7 @@ treataslocal:
 		}
 		rpt_mutex_lock(&myrpt->lock);
 		/* get all the nodes */
-		n = __mklinklist(myrpt, NULL, &lbuf, 0) + 1;
+		n = __mklinklist(myrpt, NULL, &lbuf, USE_FORMAT_RPT_LINK) + 1;
 		rpt_mutex_unlock(&myrpt->lock);
 		strs = ast_malloc(n * sizeof(char *));
 		if (!strs) {
@@ -3153,7 +3153,7 @@ void rpt_telemetry(struct rpt *myrpt, enum rpt_tele_mode mode, void *data)
 			rpt_mutex_lock(&myrpt->lock);
 			snprintf(mystr, sizeof(mystr), "STATUS,%s,%d", myrpt->name, myrpt->callmode);
 			/* get all the nodes */
-			n = __mklinklist(myrpt, NULL, &lbuf, 0) + 1;
+			n = __mklinklist(myrpt, NULL, &lbuf, USE_FORMAT_RPT_LINK) + 1;
 			rpt_mutex_unlock(&myrpt->lock);
 			strs = ast_malloc(n * sizeof(char *));
 			if (!strs) {

commit 38bc07e37dbdbc8c6a9b3f8f8fef3e3140dc8e06

app_rpt: statpost should only report adjacent nodes
This change corrects a regression that was introduced with #933.

apps/app_rpt.c
@@ -3307,7 +3307,7 @@ static inline void periodic_process_link(struct rpt *myrpt, struct rpt_link *l, 
 		l->linklisttimer = myrpt->p.linkpost_time * 1000;
 		ast_str_set(&lstr, 0, "%s", "L ");
 		rpt_mutex_lock(&myrpt->lock);
-		__mklinklist(myrpt, l, &lstr, 0);
+		__mklinklist(myrpt, l, &lstr, USE_FORMAT_RPT_LINK);
 		rpt_mutex_unlock(&myrpt->lock);
 		if (l->chan) {
 			struct ast_frame lf = {

apps/app_rpt/rpt_link.c
@@ -519,6 +519,10 @@ int __mklinklist(struct rpt *myrpt, struct rpt_link *mylink, struct ast_str **bu
 			/* add the adjacent node */
 			ast_str_append(buf, 0, "%s%c%s", sep, mode, l->name);
 
+			if (flags & USE_FORMAT_RPT_LINKPOST) {
+				continue;
+			}
+
 			/* and append any nodes linked to the adjacent node */
 			str = ast_str_buffer(l->linklist);
 			len = ast_str_strlen(l->linklist);
@@ -557,10 +561,6 @@ int __mklinklist(struct rpt *myrpt, struct rpt_link *mylink, struct ast_str **bu
 					break;
 				}
 			}
-		}
-
-		if (flags & USE_FORMAT_RPT_LINKPOST) {
-			continue;
 		}
 
 		if (mode == 'T') {