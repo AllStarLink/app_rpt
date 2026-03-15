032fbc83098b522e57c2b31f8ae835e2f81bb0d2
mkmer <7760516+mkmer@users.noreply.github.com>
2026-03-15 08:01:42 -0400

    Document HAMVOIP time limit in rpt_config.c
    Added comment regarding HAMVOIP time limit for linkpost_time.

--- apps/app_rpt/rpt_config.c
+++ apps/app_rpt/rpt_config.c
@@ -884,6 +884,9 @@ void load_rpt_vars(int n, int init)
 		/* if message truncation enabled, set minimum */
 		rpt_vars[n].p.linkpost_max_message_len = 500;
 	}
+	/* Due to a limit imposed by HAMVOIP, 40 seconds is the maximum time allowed
+	 * Without the HAMVOIP limitation, the uppder limit could be increased in the future 
+	 */
 	RPT_CONFIG_VAR_INT_DEFAULT_MIN_MAX(linkpost_time, "linkpost_time", 30, 10, 40);
 
 	/* configure how we interact with "stats.allstarlink.org" */
