2c08f0c55cdcfea58fe23cbfa88833130913ed0a
mkmer <m_kasper@sbcglobal.net>
2026-04-12 09:20:28 -0400

    white space

--- res/res_rpt_http_registrations.c
+++ res/res_rpt_http_registrations.c
@@ -600,5 +600,5 @@ AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_DEFAULT, "RPT HTTP Periodic Regist
 	.load = load_module,
 	.unload = unload_module,
 	.reload = reload_module,
-	.requires = "res_curl", 
+	.requires = "res_curl",
 );
