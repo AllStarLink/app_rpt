129e72cae74634daa9419211b04b0c717f94cccd
root <chuck@topsail.org>
2026-04-25 18:17:27 -0700

    attempt to suppress warning

--- apps/app_rpt/rpt_serial.c
+++ apps/app_rpt/rpt_serial.c
@@ -7,25 +7,7 @@
 
 #include "asterisk.h"
 
-#include <stdio.h>
-#include <ctype.h>
-#include <math.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
-#include <fcntl.h>
-#include <sys/time.h>
-#include <stdlib.h>
 #include <termios.h>
-#include <errno.h>
-#include <usb.h>
-#include <linux/ppdev.h>
-#include <linux/parport.h>
-#include <linux/version.h>
-
-#ifdef HAVE_SYS_IO
-#include <sys/io.h>
-#endif
 
 #include "asterisk/utils.h"
 #include "asterisk/lock.h"
@@ -470,6 +452,8 @@ static void rbi_out_parallel(struct rpt *myrpt, unsigned char *data)
 	}
 	/* >= 50 us */
 	usleep(50);
+#else
+	;
 
 #endif /* HAVE_SYS_IO */
 }
