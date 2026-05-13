89e9ea54b0e680ed117ecde9cd5b30b81d800d48
Mike N8RAW <7760516+mkmer@users.noreply.github.com>
2026-05-13 15:03:01 -0400

    Update channels/chan_echolink.c
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- channels/chan_echolink.c
+++ channels/chan_echolink.c
@@ -491,7 +491,7 @@ struct el_pvt {
 	int rxkey;						/* Receive keyed timer */
 	int keepalive;
 	int txindex;
-	struct ast_timer *timer; /* Timer for receive audio */	
+	struct ast_timer *timer; /* Timer for receive audio */
 	struct el_rxqast rxqast; /* Received data queue */
 	struct ast_dsp *dsp;
 	struct ast_module_user *u;
