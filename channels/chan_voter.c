db87e8e2b7fc7b47006d1bec303f6734a50bfe21
Mike N8RAW <7760516+mkmer@users.noreply.github.com>
2026-08-18 11:00:33 -0400

    chan_voter: Now requires res_usbradio (#1199)

--- channels/chan_voter.c
+++ channels/chan_voter.c
@@ -6531,4 +6531,5 @@ AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_DEFAULT, "Voter Radio Channel Driv
 	.load = load_module,
 	.unload = unload_module,
 	.reload = reload_module,
+	.requires = "res_usbradio",
 );
