30cf96b931009694f1cc9be7bca26abe124e4e9d
mkmer <m_kasper@sbcglobal.net>
2026-09-09 13:54:01 -0400

    chan_voter.c: Bump CLIENT_TIMEOUT_MS to 4600 allowing for 2 missed packets before resetting.

--- channels/chan_voter.c
+++ channels/chan_voter.c
@@ -284,7 +284,7 @@ static char context[AST_MAX_EXTENSION] = "default";
 
 /* Timeout definitions in ms */
 #define RX_TIMEOUT_MS 200
-#define CLIENT_TIMEOUT_MS 3000
+#define CLIENT_TIMEOUT_MS 4600 /* GPS_FORCE_TIME in RTCM is 1500ms,  1500*3 = 4500 for 2 missed packets + 100ms for jitter */
 #define MASTER_TIMEOUT_MS 100
 #define TX_KEEPALIVE_MS 1000
 #define PING_TIME_MS 250
