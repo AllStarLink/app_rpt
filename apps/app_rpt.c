2f2e7273bd6179cf0a34f23c2bd22d221157212f
mkmer <m_kasper@sbcglobal.net>
2026-09-15 15:00:50 -0400

    app_rpt.c: Make sure audiohook is running before adding frames, remove unnecessary lock

--- apps/app_rpt.c
+++ apps/app_rpt.c
@@ -5144,10 +5144,9 @@ static inline int monchannel_read(struct rpt *myrpt)
 			 * whisper the output audio onto said link.
 			 */
 			if (l->chan && altlink(myrpt, l) && (!l->lastrx) && (!myrpt->remrx) && (!myrpt->keyed) &&
-				((l->link_newkey != RADIO_KEY_NOT_ALLOWED) || l->lasttx || !CHAN_TECH(l->chan, "IAX2"))) {
-				ast_audiohook_lock(&l->altaudio);
+				((l->link_newkey != RADIO_KEY_NOT_ALLOWED) || l->lasttx || !CHAN_TECH(l->chan, "IAX2")) &&
+				l->altaudio.status == AST_AUDIOHOOK_STATUS_RUNNING) {
 				ast_audiohook_write_frame(&l->altaudio, AST_AUDIOHOOK_DIRECTION_READ, f);
-				ast_audiohook_unlock(&l->altaudio);
 			}
 		}
 		ao2_iterator_destroy(&l_it);
