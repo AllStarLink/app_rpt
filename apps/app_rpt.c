832b3381455a006ae10d5b0149465bb36d55d8c4
mkmer <m_kasper@sbcglobal.net>
2026-09-15 15:58:37 -0400

    Restore lock

--- apps/app_rpt.c
+++ apps/app_rpt.c
@@ -5147,11 +5147,9 @@ static inline int monchannel_read(struct rpt *myrpt)
 			if (l->chan && altlink(myrpt, l) && (!l->lastrx) && (!myrpt->remrx) && (!myrpt->keyed) &&
 				((l->link_newkey != RADIO_KEY_NOT_ALLOWED) || l->lasttx || !CHAN_TECH(l->chan, "IAX2")) &&
 				l->altaudio.status == AST_AUDIOHOOK_STATUS_RUNNING) {
-				ast_audiohook_unlock(&l->altaudio);
 				ast_audiohook_write_frame(&l->altaudio, AST_AUDIOHOOK_DIRECTION_READ, f);
-			} else {
-				ast_audiohook_unlock(&l->altaudio);
 			}
+			ast_audiohook_unlock(&l->altaudio);
 		}
 		ao2_iterator_destroy(&l_it);
 		rpt_mutex_unlock(&myrpt->lock);
